74a2405d23ded5b49aa4aa8a38766897afb21b2a
Devisree-Arepalli <73283630+Devisree-Arepalli@users.noreply.github.com>
2023-12-03 19:14:08 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import Table11 from "./Table";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <Table11 />
+      <ToastContainer />
+    </div>
+  );
+}
+
